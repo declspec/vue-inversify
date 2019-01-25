4643bbd9f167c7314d72d331486c4a4f50e87077
Jason Larke <jason.larke@curtin.edu.au>
2019-01-25 09:19:46 +0800

    Exported the options type, updated package.json to add a 'files' entry

--- bin/types/index.d.ts
+++ bin/types/index.d.ts
@@ -1,9 +1,8 @@
 import 'reflect-metadata';
 import { interfaces } from 'inversify';
 import Vue, { VueConstructor } from 'vue';
-declare type VueInversifyOptions = {
+export declare type VueInversifyOptions = {
     container: interfaces.Container;
 };
 export default function VueInversify(instance: VueConstructor<Vue>, options?: VueInversifyOptions): void;
 export declare function inject(identifier?: interfaces.ServiceIdentifier<any>): (target: Vue, prop: string) => void;
-export {};
