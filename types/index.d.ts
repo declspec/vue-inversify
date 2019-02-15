a2dc3c96432b9eb4fd8503c9de58a6019037dd3d
Jason Larke <jason.larke@curtin.edu.au>
2019-02-15 13:59:27 +0800

    Inlined module exports so that they're in the generated `index.d.ts`

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,15 @@
 import { interfaces } from 'inversify';
 import Vue, { PluginFunction } from 'vue';
+declare module "vue/types/options" {
+    interface ComponentOptions<V extends Vue> {
+        container?: interfaces.Container;
+    }
+}
+declare module "vue/types/vue" {
+    interface Vue {
+        $container?: interfaces.Container;
+    }
+}
 declare const VueInversify: PluginFunction<any>;
 declare const inject: (identifier?: string | symbol | interfaces.Newable<any> | interfaces.Abstract<any> | undefined) => (target: Vue, prop: string) => void;
 export default VueInversify;
