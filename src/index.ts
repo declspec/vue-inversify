9885f67dace3fbcfcf7d61c53a57c505fad18b79
Murdats <murdats@gmail.com>
2021-05-06 14:18:57 +0800

    Update inject attribute to use vue-class-component decorators to correctly work with class components and fix issue with shared global dependency registration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { interfaces } from 'inversify';
-import { ComponentOptions } from 'vue/types/options';
 import { PluginFunction } from 'vue/types/plugin';
-import { Vue, VueConstructor } from 'vue/types/vue';
+import { Vue } from 'vue/types/vue';
+import { createDecorator } from 'vue-class-component'
 
 declare const Reflect: {
     getMetadata(metadataKey: string, target: unknown, propertyKey: string): unknown;
@@ -9,31 +9,31 @@ declare const Reflect: {
 
 declare module "vue/types/options" {
     interface ComponentOptions<V extends Vue> {
-            container?: interfaces.Container;
-            dependencies?: { [key: string]: interfaces.ServiceIdentifier<unknown> };
+        container?: interfaces.Container;
+        dependencies?: { [key: string]: interfaces.ServiceIdentifier<unknown> };
     }
 }
-
-type InjectableVueClass = VueConstructor<Vue> & {
-    options: ComponentOptions<Vue>
-};
 
 const VueInversify: PluginFunction<unknown> = instance => {
     instance.mixin({
         beforeCreate() {
             const dependencies = this.$options.dependencies;
-            const container = this.$options.container || (this.$options.container = getContainer(this));
             
             if (dependencies) {
+                const container = this.$options.container || (this.$options.container = getContainer(this));
+
                 if (!container)
                     throw new TypeError('vue-inversify: encountered props marked with @inject but no container was provided when creating the Vue instance');
-
+                
                 Object.keys(dependencies).forEach(key => {
+                    if (Object.getOwnPropertyDescriptor(this, key))
+                        return;
+
                     Object.defineProperty(this, key, {
                         enumerable: true,
                         value: container.get(dependencies[key])
                     });
-                });
+                });            
             }
         }
     });
@@ -41,22 +41,21 @@ const VueInversify: PluginFunction<unknown> = instance => {
 
 const inject = (identifier?: interfaces.ServiceIdentifier<unknown>) => {
     return function(target: Vue, prop: string) {
-        // Try use reflection to generate the identifier if not provided explicitly.
-        if (!identifier && typeof(Reflect) !== 'undefined' && typeof(Reflect.getMetadata) === 'function')
-            identifier = Reflect.getMetadata('design:type', target, prop) as interfaces.ServiceIdentifier<unknown>;
+        createDecorator((options, key) => {
+            // Try use reflection to generate the identifier if not provided explicitly.
+            if (!identifier && typeof(Reflect) !== 'undefined' && typeof(Reflect.getMetadata) === 'function')
+                identifier = Reflect.getMetadata('design:type', target, prop) as interfaces.ServiceIdentifier<unknown>;
 
-        if (!identifier)
-            throw new TypeError('Unable to resolve identifier; please specify it explicitly');
+            if (!identifier)
+                throw new TypeError('vue-inversify: Unable to resolve identifier; please specify it explicitly');
 
-        const injectable = (typeof(target) === 'function' ? target : target.constructor) as InjectableVueClass;
-        const options = injectable.options || (injectable.options = {});
-        const dependencies = options.dependencies || (options.dependencies = {});
-
-        dependencies[prop] = identifier;
+            const dependencies = options.dependencies || (options.dependencies = {});
+            dependencies[key] = identifier;
+        })(target, prop);
     }
 };
 
-function getContainer(component: Vue): interfaces.Container | undefined {
+function getContainer(component: Vue) {
     let container: interfaces.Container | undefined;
 
     while (component != null && !container) {
